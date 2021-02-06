beb2d14199fce34ad9388c692724ff6cb71ebaba
MirajHoque <a.h.miraj2031@gmail.com>
2021-02-06 22:56:06 +0600

    vue files
    root component

--- src/main.js
+++ src/main.js
@@ -1,7 +1,13 @@
 import Vue from 'vue'
+//import vue in itself 
 import App from './App.vue'
+//import Hi from './App.vue'
 
 new Vue({
   el: '#app',
-  render: h => h(App)
+  
+  render: h => h(App) //render App.vue with id #app
+  //render: h => h(Hi)
+  //render method:
+  // h=>h(basically rendering App.vue to el:'#app')
 })
