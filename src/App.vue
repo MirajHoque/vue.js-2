353e634d4ae44aef141b41652f8d0cf111409e66
MirajHoque <a.h.miraj2031@gmail.com>
2021-02-17 22:22:33 +0600

    nesting component
    app remain as root component

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,29 @@
 <template>
-  <div>
-    <h1>{{title}}</h1>
-    <P>{{greeting()}}</P>
-   
-  </div>
+<div>
+  <app-header></app-header>
+  <app-ninjas></app-ninjas>
+  <app-footer></app-footer>
+</div>
 </template>
 
 <script>
+import Header from './components/Header.vue';
+import Footer from './components/Footer.vue';
+import Ninjas from './components/Ninjas.vue';
 export default {
+  components:{
+    'app-header':Header,
+    'app-footer':Footer,
+    'app-ninjas':Ninjas
+  },
   data () {
-    return {
-      title: 'Vue.js 2'
+    return{
 
     }
-  },
-  methods:{
-    greeting:function(){
-      return 'Hello vue'
+
     }
   }
 
-}
 </script>
 
 <style>
